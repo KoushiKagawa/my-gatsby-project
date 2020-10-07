97bb586ab3fbe2a9ee98eee333ee855d5da759f4
KoushiKagawa <koushi.kagawa@resinc.co.jp>
2020-10-07 17:19:12 +0900

    2020107bog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `KAGAWA Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Koushi Kagawa`,
+      summary: `who lives and works in Matsumoto-city`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `KAGAWA Blog by GatsbyJS`,
+    siteUrl: `https://kagawa.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `koushikagawa`,
     },
   },
   plugins: [
